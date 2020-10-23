5406054bf113ea524a5a78719d6ecf3cc9406f5c
somercic1 <somercic1@etf.unsa.ba>
2020-10-23 23:34:15 +0200

    Program se kompajlira

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,36 +1,42 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
 
-public class Korpa extends Supermarket {
+
+public class Korpa  {
     Artikl[] korpa = new Artikl[50];
 
-    public boolean dodajArtikl(Artikl a) {
 
-
-       if()
-            return true;
-        else
-            return false;
-    }
 
     public Artikl[] getArtikli() {
         return this.korpa;
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
-        Artikl izbaceni;
-        for (int i = 0; i < this.korpa.length(); i++) {
-            if (this.korpa[i].kod == kod)
-                izbaceni=this.korpa[i];
+        Artikl izbaceni=null;
+        for (int i = 0; i < this.korpa.length; i++) {
+            if (this.korpa[i].kod.equals(kod))
+                return this.korpa[i];
         }
         return izbaceni;
     }
 
+    public boolean dodajArtikl(Artikl biciklo) {
+        for(int i=0; i < this.korpa.length; i++){
+            if(this.korpa[i]==null) {
+                this.korpa[i] = biciklo;
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int dajUkupnuCijenuArtikala() {
         int ukCijena=0;
-        for(int i=0; i< korpa.length(); i++)
-            ukCijena=ukCijena+korpa[i].cijena;
+        for(int i=0; i < this.korpa.length; i++){
+            if(this.korpa[i]!=null)
+                ukCijena=ukCijena+korpa[i].cijena;
+        }
+
         return ukCijena;
     }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -10,16 +10,22 @@ public class Supermarket {
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
-        Artikl izbaceni;
-        for (int i = 0; i < this.artikli.length(); i++) {
-            if (this.artikli[i].kod == kod)
-                izbaceni=this.artikli[i];
+        Artikl prazan=null;
+        for (int i = 0; i < this.artikli.length; i++) {
+            if (this.artikli[i].kod.equals(kod))
+                return this.artikli[i];
         }
-        return izbaceni;
+        return prazan;
     }
 
-    public void dodajArtikl(Artikl biciklo) {
-
+    public boolean dodajArtikl(Artikl biciklo) {
+        for(int i=0;i<artikli.length; i++){
+            if(artikli[i]==null) {
+                artikli[i] = biciklo;
+                return true;
+            }
+        }
+        return false;
     }
 }
 
